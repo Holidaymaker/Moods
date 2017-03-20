53025dc0599dfd6d5e66226ec95f7f4cd4041457
Trace Rawl <tracerawl@gmail.com>
2017-03-19 18:52:22 -0600

    jQuery at bottom
    For background color animation.

--- moods.js
+++ moods.js
@@ -185,3 +185,12 @@ function setSong(songPlayer) {
     }, true);
 }
 
+$( "#plus, #minus" ).on('click', function() {
+  $( "#hi" ).animate({
+    color: moods[currentPlaylist].textColor,
+    backgroundColor: moods[currentPlaylist].color,
+    transition: ".3s ease-in-out"
+    
+  });
+});
+
