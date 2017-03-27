04e4771267c98b65e7cd0d7570be35bb63dd1854
Matthew Abraham <mattabraham4@gmail.com>
2017-03-27 14:59:42 -0600

    Song objects initialized in their own file

---
+++ playlists.js
@@ -0,0 +1,103 @@
+// Playlist constructor function, determines properties of playlist objects
+function Playlist(playlistName, songList, playlistURL) {
+  this.playlistName = playlistName;
+  this.songList = songList;
+  this.playlistURL = playlistURL;
+}
+
+// initializes each playlist object
+var shhhh = new Playlist(
+    'Shhhh',
+    [iota],
+    "https://play.spotify.com/user/1225101838/playlist/1RYBHqNl2vL0VBEEktfoIn"
+);
+var mhmmmm = new Playlist(
+    'Mhmmmm',
+    [redford],
+    "https://play.spotify.com/user/1225101838/playlist/1uyCOXWEEWp2FedngsjTUZ"
+);
+var darkBlue = new Playlist(
+    'Dark Blue',
+    [spiritFuture],
+    "https://play.spotify.com/user/1225101838/playlist/00RFVXED4CDE6NHCuU7ENW"
+);
+var dreamin = new Playlist(
+    'Dreamin',
+    [introCosmicDrama],
+    "https://play.spotify.com/user/1225101838/playlist/5tDsc9fYFWSmfdWMFeErVv"
+);
+var mutedYe = new Playlist(
+    'Muted, Ye',
+    [hiking],
+    "https://play.spotify.com/user/1225101838/playlist/2frW4RaubHbVlPJ41qTDx9"
+);
+var mellows = new Playlist(
+    'Mellows',
+    [starsKeepCalling],
+    "https://play.spotify.com/user/1225101838/playlist/3cvUawxH5mPK8V5YDgs9vv"
+);
+var feels = new Playlist(
+    'Feels',
+    [theseDays],
+    "https://play.spotify.com/user/1225101838/playlist/3WA5q1hfSCMKGNjDAPsQ0M"
+);
+var terminallyChill = new Playlist(
+    'Terminally Chill',
+    [huey],
+    "https://play.spotify.com/user/1225101838/playlist/3ywVIrKgnzDW5QNbklinFM"
+);
+var steadied = new Playlist(
+    'Steadied',
+    [herMajesty],
+    "https://play.spotify.com/user/1225101838/playlist/1w6hlLnRvGTxYzjyDUwAX3"
+);
+var schemin = new Playlist(
+    'Schemin',
+    [rainySouls],
+    "https://play.spotify.com/user/1225101838/playlist/70yfzw4kIL4vmVT3XbApwz"
+);
+var elevated = new Playlist(
+    'Elevated',
+    [noSass],
+    "https://play.spotify.com/user/1225101838/playlist/7h6JG4j25QwotuZiUm4kxA"
+);
+var groovin = new Playlist(
+    'Groovin',
+    [already],
+    "https://play.spotify.com/user/1225101838/playlist/7H2JGSkIe3f8Qjt31aHnUF"
+);
+var vibesForWeeks = new Playlist(
+    'Vibes for Weeks',
+    [deadbeatSummer],
+    "https://play.spotify.com/user/1225101838/playlist/6tsGco49f1TGkgrUbNrtZz"
+);
+var stirThePot = new Playlist(
+    'Stir the Pot',
+    [excuseMe],
+    "https://play.spotify.com/user/1225101838/playlist/3NxYrfTGESu3DnTfOWC4Kg"
+);
+var upAndAway = new Playlist(
+    'Up & Away',
+    [cousins],
+    "https://play.spotify.com/user/1225101838/playlist/1qVSR1cBf0O68ftEV55ZV7"
+);
+var blurred = new Playlist(
+    'Blurred',
+    [brokeup],
+    "https://play.spotify.com/user/1225101838/playlist/3FllHOyay1CvFAuAUKAlTh"
+);
+var headDancin = new Playlist(
+    'Head Dancin',
+    [liteSpots],
+    "https://play.spotify.com/user/1225101838/playlist/0wnBjKJ7jydQuydf4S7ftf"
+);
+var twistTheKnife = new Playlist(
+    'Twist the Knife',
+    [youMightThink],
+    "https://play.spotify.com/user/1225101838/playlist/5YsHC2XQdbYjGIiKYj28ag"
+);
+var wooooo = new Playlist(
+    'Wooooo',
+    [whoWantsToRock],
+    "https://play.spotify.com/user/1225101838/playlist/18NtDX0lTdCKJ8ohf2dEIf"
+);
