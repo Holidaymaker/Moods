2e8dbc073aa23288939df04862e0ce742e10adc0
Matthew Abraham <mattabraham4@gmail.com>
2017-05-14 11:05:45 -0600

    Added urls to each song

---
+++ songs_mvp.js
@@ -0,0 +1,140 @@
+function Song(artistName, songName, songSource) {
+  this.artistName = artistName;
+  this.songName = songName;
+  this.songSource = songSource;
+}
+
+// initializes each song object
+// shhhh
+var evaporar = new Song(
+  'Little Joy',
+  'Evaporar',
+  "https://p.scdn.co/mp3-preview/0c068b90f07a6ffe469fe183266e4dd0394994e8?cid=null"
+);
+
+// mhmmmm
+var swingLoUMO = new Song(
+  'Unknown Mortal Orchestra',
+  'Swing Lo Magellen (Dirty Projectors Cover)',
+  "https://p.scdn.co/mp3-preview/3087b9cc169857500a77f9612fee1ee34605476f?cid=null"
+);
+
+// darkBlue
+var unftheworld = new Song(
+  'Angel Olsen',
+  'Unf***theworld',
+  "https://p.scdn.co/mp3-preview/a8164f2d6175d084edb9fb15b758a00610a3cc3c?cid=null"
+);
+
+// dreamin
+var introCosmicDrama = new Song(
+  'Flying Lotus',
+  'Intro//A Cosmic Drama',
+  "https://p.scdn.co/mp3-preview/4f6855ecaf57d2d955e1445c1661d70fc155f526?cid=null"
+);
+
+// mutedYe
+var hiking = new Song(
+  'Mndsgn',
+  'Hiking',
+  "https://p.scdn.co/mp3-preview/c19c90cb21dc0edf5543348d1e8ad3c5a4ed531d?cid=null"
+);
+
+// mellows
+var blueBoy = new Song(
+  'Mac Demarco',
+  'Blue Boy',
+  'https://p.scdn.co/mp3-preview/9a5dffe57e62198438344076571b11e2e68c1aca?cid=null'
+);
+
+// feels
+var theseDays = new Song(
+  'Niko',
+  'These Days',
+  "https://p.scdn.co/mp3-preview/365a79c64bf2fde43757dea7ef3b1483329eabe2?cid=null"
+);
+
+// terminallyChill
+var delMantel = new Song(
+  'Bronko Yotte',
+  'Del Mantel',
+  "https://p.scdn.co/mp3-preview/f88070ca609d798e3ce1dce89c036c5a4f8ed910?cid=null"
+);
+
+// steadied
+var lostMyHead = new Song(
+  'Kurt Vile',
+  'Lost my Head there',
+  'https://p.scdn.co/mp3-preview/2e79d9a21477a7329ecce3252f3cd11bb403ebd1?cid=null'
+);
+
+
+// schemin
+var rainySouls = new Song(
+  'Clap! Clap!',
+  'Rainy Souls, Gloomy Futures',
+  'https://p.scdn.co/mp3-preview/bcb6a652933539b91b47c5551a0c8896a0eb2566?cid=null'
+);
+
+// elevated
+var zottiU = new Song(
+  'Zotti',
+  'U',
+  'https://p.scdn.co/mp3-preview/d8aa2283e1361dc492d5c246051cd34534259aaa?cid=null'
+);
+
+// groovin
+var already = new Song(
+  'Anderson .Paak',
+  'Already',
+  "https://p.scdn.co/mp3-preview/dba20eb4e3d3a6dfb8100f607011b988865a5e9d?cid=null"
+);
+
+// vibesForWeeks
+var deadbeatSummer = new Song(
+  'Neon Indian',
+  'Deadbeat Summer',
+  'https://p.scdn.co/mp3-preview/427de3ae88745dc35ee9e6737bd10b209b4f5699?cid=null'
+);
+
+// stirThePot
+var melaninDrop = new Song(
+  'Cities Aviv',
+  'Melanin Drop',
+  'https://p.scdn.co/mp3-preview/061c33e5d0e1c47ad7ed499731891a166060b6d0?cid=null'
+);
+
+// upAndAway
+var cousins = new Song(
+  'Vampire Weekend',
+  'Cousins',
+  'https://p.scdn.co/mp3-preview/bed57f48e889e0142408187ceaadb0036b13cc56?cid=null'
+);
+
+// blurred
+var brokeup = new Song(
+  'Arca',
+  'Brokeup',
+  'https://p.scdn.co/mp3-preview/90351beb3a2c4cd26bb65eb8b9db96fbdb4cb14f?cid=null'
+);
+
+// headDancin
+var liteSpots = new Song(
+  'KAYTRANADA',
+  'LITE SPOTS',
+  "https://p.scdn.co/mp3-preview/654e7c82ae94db23940cc2452a03dab8d1605f6b?cid=null"
+);
+
+// twistTheKnife
+var youMightThink = new Song(
+  'Death Grips',
+  'You Might Think He Loves You for Your Money but I Know What He Really Loves You for It’s Your Brand New Leopard Skin Pillbox Hat',
+  'https://p.scdn.co/mp3-preview/1d7b2d3f3202cd0cfc84962e25fa1e8b42e13512?cid=null'
+);
+
+// wooooo
+var higherGround = new Song(
+  'TNGHT',
+  'Higher Ground',
+  "https://p.scdn.co/mp3-preview/09f5a02ed13e656bc1d6933dd6a86ea6279db01c?cid=null"
+);
