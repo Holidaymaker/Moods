f96f544b78092b26844583d9c33b84262051e3f4
Trace Rawl <tracerawl@gmail.com>
2017-05-20 16:00:28 -0600

    Adding css classes, descriptions, and cover art

--- playlists_mvp.js
+++ playlists_mvp.js
@@ -3,101 +3,162 @@ function Playlist(playlistName, songList, playlistURL) {
   this.playlistName = playlistName;
   this.songList = songList;
   this.playlistURL = playlistURL;
+  this.cssClass = cssClass;
+  this.description = description;
+  this.cover = cover;
 }
 
 // initializes each playlist object
 var shhhh = new Playlist(
     'Shhhh',
     [evaporar],
-    "https://play.spotify.com/user/1225101838/playlist/1RYBHqNl2vL0VBEEktfoIn"
+    "https://play.spotify.com/user/1225101838/playlist/1RYBHqNl2vL0VBEEktfoIn",
+    'shhhh',
+    'hhh..',
+    'moodsart/shhhh.jpg'
 );
 var morningTea = new Playlist(
     'Morning Tea',
     [swingLoUMO],
-    "https://play.spotify.com/user/1225101838/playlist/1uyCOXWEEWp2FedngsjTUZ"
+    "https://play.spotify.com/user/1225101838/playlist/1uyCOXWEEWp2FedngsjTUZ",
+    'mhmmmm',
+    'Gentle sunrise music.',
+    'moodsart/mhmmmm.jpg'
 );
 var darkBlue = new Playlist(
     'Dark Blue',
     [unftheworld],
-    "https://play.spotify.com/user/1225101838/playlist/00RFVXED4CDE6NHCuU7ENW"
+    "https://play.spotify.com/user/1225101838/playlist/00RFVXED4CDE6NHCuU7ENW",
+    'darkBlue',
+    'Despair to get on with it.',
+    'moodsart/darkBlue.jpg'
 );
 var dreamin = new Playlist(
     'Dreamin',
     [introCosmicDrama],
-    "https://play.spotify.com/user/1225101838/playlist/5tDsc9fYFWSmfdWMFeErVv"
+    "https://play.spotify.com/user/1225101838/playlist/5tDsc9fYFWSmfdWMFeErVv",
+    'dreamin',
+    'Spacy lullabies.',
+    'moodsart/dreamin.jpg'
 );
 var mutedYe = new Playlist(
     'Muted, Ye',
     [hiking],
-    "https://play.spotify.com/user/1225101838/playlist/2frW4RaubHbVlPJ41qTDx9"
+    "https://play.spotify.com/user/1225101838/playlist/2frW4RaubHbVlPJ41qTDx9",
+    'mutedYe',
+    'Subdued.',
+    'moodsart/mutedYe.jpg'
+  
 );
 var mellows = new Playlist(
     'Mellows',
     [blueBoy],
-    "https://play.spotify.com/user/1225101838/playlist/3cvUawxH5mPK8V5YDgs9vv"
+    "https://play.spotify.com/user/1225101838/playlist/3cvUawxH5mPK8V5YDgs9vv",
+    'mellows',
+    'Calm & Cheery',
+    'moodsart/mellows.jpg'
 );
 var feels = new Playlist(
     'Feels',
     [theseDays],
-    "https://play.spotify.com/user/1225101838/playlist/3WA5q1hfSCMKGNjDAPsQ0M"
+    "https://play.spotify.com/user/1225101838/playlist/3WA5q1hfSCMKGNjDAPsQ0M",
+    'feels',
+    'Right in em.',
+    'moodsart/feels.jpg'
 );
 var terminallyChill = new Playlist(
     'Terminally Chill',
     [delMantel],
-    "https://play.spotify.com/user/1225101838/playlist/3ywVIrKgnzDW5QNbklinFM"
+    "https://play.spotify.com/user/1225101838/playlist/3ywVIrKgnzDW5QNbklinFM",
+    'terminallyChill',
+    'The start of a good unwind.',
+    'moodsart/terminallyChill.jpg'
 );
 var steadied = new Playlist(
     'Steadied',
     [lostMyHead],
-    "https://play.spotify.com/user/1225101838/playlist/1w6hlLnRvGTxYzjyDUwAX3"
+    "https://play.spotify.com/user/1225101838/playlist/1w6hlLnRvGTxYzjyDUwAX3",
+    'steadied',
+    'The vanilla ice cream of the bunch.',
+    'moodsart/steadied.jpg'
 );
 var schemin = new Playlist(
     'Schemin',
     [rainySouls],
-    "https://play.spotify.com/user/1225101838/playlist/70yfzw4kIL4vmVT3XbApwz"
+    "https://play.spotify.com/user/1225101838/playlist/70yfzw4kIL4vmVT3XbApwz",
+    'schemin',
+    'hhh..',
+    'moodsart/schemin.jpg'
 );
 var elevated = new Playlist(
     'Elevated',
     [zottiU],
-    "https://play.spotify.com/user/1225101838/playlist/7h6JG4j25QwotuZiUm4kxA"
+    "https://play.spotify.com/user/1225101838/playlist/7h6JG4j25QwotuZiUm4kxA",
+    'elevated',
+    'hhh..',
+    'moodsart/elevated.jpg'
 );
 var groovin = new Playlist(
     'Groovin',
     [already],
-    "https://play.spotify.com/user/1225101838/playlist/7H2JGSkIe3f8Qjt31aHnUF"
+    "https://play.spotify.com/user/1225101838/playlist/7H2JGSkIe3f8Qjt31aHnUF",
+    'groovin',
+    'If you can strut down the street to it..',
+    'moodsart/groovin.jpg'
 );
 var vibesForWeeks = new Playlist(
     'Vibes for Weeks',
     [deadbeatSummer],
-    "https://play.spotify.com/user/1225101838/playlist/6tsGco49f1TGkgrUbNrtZz"
+    "https://play.spotify.com/user/1225101838/playlist/6tsGco49f1TGkgrUbNrtZz",
+    'vibesForWeeks',
+    'Windows rolled down in the summer.',
+    'moodsart/vibesForWeeks.jpg'
 );
 var stirThePot = new Playlist(
     'Stir the Pot',
     [melaninDrop],
-    "https://play.spotify.com/user/1225101838/playlist/3NxYrfTGESu3DnTfOWC4Kg"
+    "https://play.spotify.com/user/1225101838/playlist/3NxYrfTGESu3DnTfOWC4Kg",
+    'stirThePot',
+    'Take Roy off the grid.',
+    'moodsart/stirThePot.jpg'
 );
 var upAndAway = new Playlist(
     'Up & Away',
     [cousins],
-    "https://play.spotify.com/user/1225101838/playlist/1qVSR1cBf0O68ftEV55ZV7"
+    "https://play.spotify.com/user/1225101838/playlist/1qVSR1cBf0O68ftEV55ZV7",
+    'upAndAway',
+    'The punk rocket.',
+    'moodsart/upAndAway.jpg'
 );
 var blurred = new Playlist(
     'Blurred',
     [brokeup],
-    "https://play.spotify.com/user/1225101838/playlist/3FllHOyay1CvFAuAUKAlTh"
+    "https://play.spotify.com/user/1225101838/playlist/3FllHOyay1CvFAuAUKAlTh",
+    'blurred',
+    'The lines, falling out of focus, dissolved the cage of longing for a moment apart, denegraded the rash imagination. Th...',
+    'moodsart/blurred.jpg'
 );
 var headDancin = new Playlist(
     'Head Dancin',
     [liteSpots],
-    "https://play.spotify.com/user/1225101838/playlist/0wnBjKJ7jydQuydf4S7ftf"
+    "https://play.spotify.com/user/1225101838/playlist/0wnBjKJ7jydQuydf4S7ftf",
+    'headDancin',
+    'Turn up beats for the New Yorker reader in your life.',
+    'moodsart/headDancin.jpg'
 );
 var twistTheKnife = new Playlist(
     'Twist the Knife',
     [youMightThink],
-    "https://play.spotify.com/user/1225101838/playlist/5YsHC2XQdbYjGIiKYj28ag"
+    "https://play.spotify.com/user/1225101838/playlist/5YsHC2XQdbYjGIiKYj28ag",
+    'twistTheKnife',
+    'hhh..',
+    'moodsart/twistTheKnife.png'
 );
 var wooooo = new Playlist(
     'Wooooo',
     [higherGround],
-    "https://play.spotify.com/user/1225101838/playlist/18NtDX0lTdCKJ8ohf2dEIf"
+    "https://play.spotify.com/user/1225101838/playlist/18NtDX0lTdCKJ8ohf2dEIf",
+    'wooooo',
+    'Best way to entertain the dozen refuse-to-turn-down drunk people in your bedroom.',
+    'moodsart/woooo.jpg'
 );
